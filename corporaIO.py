86bd4b5561a71b0e676b5e27e0b1a25a9489a2ab
John O. Brickley <obryan.brickley@gmail.com>
2013-04-04 01:14:26 -0800

    Continued corporaIO; broke off chainBuilder.

--- corporaIO.py
+++ corporaIO.py
@@ -1,8 +1,10 @@
 from music21 import *
 from music21 import corpus
+t 
 
-def thatone():
-    print "It\'s a doin\' that!"
+# loads supported files into music21 streams; music21.stream.Score objects?
+music21.converter
+def addFile(file):
 
 streamObject = converter.parse(humdrum.testFiles.mazurka6)
 stream2 = streamObject.stripTies()
@@ -14,3 +16,5 @@ for work in corpus.beethovenStringQuartets():
     if work.flat.getElementsByClass(meter.TimeSignature)[0].ratioString == '6/8':
         catalog.append(work.measureRange(0,2))
 catalog.show() 
+
+# return something interpretable by chainBuilder
