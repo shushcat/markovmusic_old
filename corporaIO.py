72fbe198cf2ebbcebb59b3eb51f0026764cc0ff1
John O. Brickley <obryan.brickley@gmail.com>
2013-04-04 01:51:18 -0800

    Successfully def'd addFile().

--- corporaIO.py
+++ corporaIO.py
@@ -1,20 +1,24 @@
 from music21 import *
 from music21 import corpus
-t 
 
 # loads supported files into music21 streams; music21.stream.Score objects?
-music21.converter
+
 def addFile(file):
+    converter.parseFile(file)
 
-streamObject = converter.parse(humdrum.testFiles.mazurka6)
-stream2 = streamObject.stripTies()
-correlated = graph.Plot3DBarsPitchSpaceQuarterLength(stream2.flat)
-correlated.process() 
+addFile('supplementary corpora/myboy.mid')
+# Hysterograms
+# streamObject = converter.parse(humdrum.testFiles.mazurka6)
+# stream2 = streamObject.stripTies()
+# correlated = graph.Plot3DBarsPitchSpaceQuarterLength(stream2.flat)
+# correlated.process() 
 
+'''
 catalog = stream.Score()
-for work in corpus.beethovenStringQuartets():
+for work in corpus.bach():
     if work.flat.getElementsByClass(meter.TimeSignature)[0].ratioString == '6/8':
         catalog.append(work.measureRange(0,2))
 catalog.show() 
+'''
 
 # return something interpretable by chainBuilder
