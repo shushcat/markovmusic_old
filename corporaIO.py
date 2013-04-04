5bb4ac11f53ad467905f40b088bce0998e1602be
John O. Brickley <obryan.brickley@gmail.com>
2013-04-04 01:01:35 -0800

    Cleaned up, added supplementary corpora.

--- corporaIO.py
+++ corporaIO.py
@@ -1,8 +1,16 @@
+from music21 import *
+from music21 import corpus
+
 def thatone():
     print "It\'s a doin\' that!"
 
-from music21 import *
 streamObject = converter.parse(humdrum.testFiles.mazurka6)
 stream2 = streamObject.stripTies()
 correlated = graph.Plot3DBarsPitchSpaceQuarterLength(stream2.flat)
 correlated.process() 
+
+catalog = stream.Score()
+for work in corpus.beethovenStringQuartets():
+    if work.flat.getElementsByClass(meter.TimeSignature)[0].ratioString == '6/8':
+        catalog.append(work.measureRange(0,2))
+catalog.show() 
