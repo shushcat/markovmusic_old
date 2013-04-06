4528e41a550acf25db33c03c2eebbe7766e00720
John O. Brickley <obryan.brickley@gmail.com>
2013-04-06 03:00:16 -0800

    Tidied up and got consistent return of pitch-list.

--- corporaIO.py
+++ corporaIO.py
@@ -1,11 +1,14 @@
 from music21 import *
 from music21 import corpus
 
-# Accepts paths to files or calls to the music21 corpora; loads supported files into music21 streams; music21.stream.Score objects?
+# Makes music21.stream.Score objects
 def addPiece(piece):
-    addFlat = converter.parseFile(piece).flat # Parses and flattens so notes can be linearly compared. Flattening precedence is: offset time, priority (low to high?), classSortOrder
-    return addFlat
+    # Parses and flattens so notes can be linearly compared. Flattening precedence is: offset time, priority (low to high?), classSortOrder
+    added = converter.parseFile(piece)
+    piecePitches = [str(p) for p in added.pitches]
+    return piecePitches
 
+# This needs to return a 12x12 matrix of note frequencies; a transition table for the particular piece.
 
 # My hysteriograms
 # streamObject = converter.parse(humdrum.testFiles.mazurka6)
@@ -15,7 +18,7 @@ def addPiece(piece):
 
 '''
 catalog = stream.Score()
-for work in corpus.bach():
+for work in corpus():
     if work.flat.getElementsByClass(meter.TimeSignature)[0].ratioString == '6/8':
         catalog.append(work.measureRange(0,2))
 catalog.show() 
