c7c1922686fd7170d06e2e3c1b577939b77142cc
John O. Brickley <obryan.brickley@gmail.com>
2013-04-07 01:46:07 -0800

    Got consistent return of ordered list; some notes omitted: why?

--- chainBuilder.py
+++ chainBuilder.py
@@ -1,9 +1,33 @@
-# First, need to combine dictionary entries by note. 
-def getFreqs(piecePitches):
-    # Dictionary of overall note frequencies per piece.
+# Gotta use regexes for this bit.
+import re
+# Build 12 key dictionary with values for note occurances.
+def pitchFreq(piecePitches):
     pitchFreqs = {}
-    for piece in piecePitches:
-        pitchFreqs[piece] = pitchFreqs.get(piece, 0) + 1
+    for pitch in piecePitches:
+        if re.search('A.', pitch):
+            pitchFreqs['A'] = pitchFreqs.get('A', 0) + 1
+        if re.search('A#.', pitch):
+            pitchFreqs['A#'] = pitchFreqs.get('A#', 0) + 1
+        if re.search('B.', pitch):
+            pitchFreqs['B'] = pitchFreqs.get('B', 0) + 1
+        if re.search('C.', pitch):
+            pitchFreqs['C'] = pitchFreqs.get('C', 0) + 1
+        if re.search('C#.', pitch):
+            pitchFreqs['C#'] = pitchFreqs.get('C#', 0) + 1
+        if re.search('D.', pitch):
+            pitchFreqs['D'] = pitchFreqs.get('D', 0) + 1
+        if re.search('D#.', pitch):
+            pitchFreqs['D#'] = pitchFreqs.get('D#', 0) + 1
+        if re.search('E.', pitch):
+            pitchFreqs['E'] = pitchFreqs.get('E', 0) + 1
+        if re.search('F.', pitch):
+            pitchFreqs['F'] = pitchFreqs.get('F', 0) + 1
+        if re.search('F#.', pitch):
+            pitchFreqs['F#'] = pitchFreqs.get('F#', 0) + 1
+        if re.search('G.', pitch):
+            pitchFreqs['G'] = pitchFreqs.get('G', 0) + 1
+        if re.search('G#.', pitch):
+            pitchFreqs['G#'] = pitchFreqs.get('G#', 0) + 1
     return pitchFreqs
 
 '''
@@ -26,19 +50,6 @@ def noteProb(noteFreqs):
 # This needs to return a 12x12 matrix of note frequencies; a transition table for the particular piece.
 
 '''
-
 def sumFreqs(pitchFreqs):
     # Some kinda regular expression search here...
-    if 'A\d'
-    # elif 'A#'
-    # elif 'B'
-    # elif 'C'
-    # elif 'C#'
-    # elif 'D'
-    # elif 'D#'
-    # elif 'E'
-    # elif 'F'
-    # elif 'F#'
-    # elif 'G'
-    # elif 'G#'
 '''
--- markovMusic.py
+++ markovMusic.py
@@ -15,16 +15,10 @@ pieceList = ['supplementary corpora/myboy.mid','supplementary corpora/WTK1.mid']
 Remember! Command line args are in sys.argv[1], sys.argv[2] ... sys.argv[0] is the script name itself and can be ignored This'll be needed it we get far enough along to allow calling the program on paths from the command line instead of hard-coding pieces.
 '''
 
-def main():
-    n = 0
-    for piece in pieceList:
-        added = corporaIO.addPiece(piece)
-        print pieceList[n]
-        print added
-        print chainBuilder.getFreqs(added)
-        n = n + 1
+n = 0
+for piece in pieceList:
+    added = corporaIO.listPitches(piece)
+    print pieceList[n]
+    print chainBuilder.pitchFreq(added)
+    n = n + 1
 
-
-# Call the main() function and run the program
-if __name__ == '__main__':
-    main()
